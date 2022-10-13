a0495e273e71f36ac08e0d7e823183130128c4a2
JOTORO <agustinlolo14@gmail.com>
2022-10-13 19:41:59 -0300

    Añadido el logo del colegio

--- Proyecto_EE501/Views/MainView.Designer.cs
+++ Proyecto_EE501/Views/MainView.Designer.cs
@@ -36,12 +36,15 @@ namespace EjemploLibreriaForms.Views
             this.btnGrupos = new System.Windows.Forms.Button();
             this.btnDocentes = new System.Windows.Forms.Button();
             this.btnAlumnos = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
             // 
-            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.BackColor = System.Drawing.Color.CadetBlue;
+            this.panel1.Controls.Add(this.pictureBox1);
             this.panel1.Controls.Add(this.btn_practicas);
             this.panel1.Controls.Add(this.btn_docs);
             this.panel1.Controls.Add(this.btn_domicilio);
@@ -56,8 +59,11 @@ namespace EjemploLibreriaForms.Views
             // 
             // btn_practicas
             // 
+            this.btn_practicas.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_practicas.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.btn_practicas.FlatStyle = System.Windows.Forms.FlatStyle.System;
             this.btn_practicas.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_practicas.Location = new System.Drawing.Point(3, 416);
+            this.btn_practicas.Location = new System.Drawing.Point(0, 439);
             this.btn_practicas.Name = "btn_practicas";
             this.btn_practicas.Size = new System.Drawing.Size(200, 48);
             this.btn_practicas.TabIndex = 5;
@@ -67,8 +73,11 @@ namespace EjemploLibreriaForms.Views
             // 
             // btn_docs
             // 
+            this.btn_docs.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_docs.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.btn_docs.FlatStyle = System.Windows.Forms.FlatStyle.System;
             this.btn_docs.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_docs.Location = new System.Drawing.Point(0, 347);
+            this.btn_docs.Location = new System.Drawing.Point(0, 373);
             this.btn_docs.Name = "btn_docs";
             this.btn_docs.Size = new System.Drawing.Size(200, 37);
             this.btn_docs.TabIndex = 4;
@@ -78,8 +87,11 @@ namespace EjemploLibreriaForms.Views
             // 
             // btn_domicilio
             // 
+            this.btn_domicilio.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_domicilio.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.btn_domicilio.FlatStyle = System.Windows.Forms.FlatStyle.System;
             this.btn_domicilio.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_domicilio.Location = new System.Drawing.Point(0, 128);
+            this.btn_domicilio.Location = new System.Drawing.Point(0, 180);
             this.btn_domicilio.Name = "btn_domicilio";
             this.btn_domicilio.Size = new System.Drawing.Size(200, 37);
             this.btn_domicilio.TabIndex = 3;
@@ -89,8 +101,11 @@ namespace EjemploLibreriaForms.Views
             // 
             // btnGrupos
             // 
+            this.btnGrupos.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnGrupos.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.btnGrupos.FlatStyle = System.Windows.Forms.FlatStyle.System;
             this.btnGrupos.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnGrupos.Location = new System.Drawing.Point(0, 280);
+            this.btnGrupos.Location = new System.Drawing.Point(0, 308);
             this.btnGrupos.Name = "btnGrupos";
             this.btnGrupos.Size = new System.Drawing.Size(200, 37);
             this.btnGrupos.TabIndex = 2;
@@ -100,8 +115,11 @@ namespace EjemploLibreriaForms.Views
             // 
             // btnDocentes
             // 
+            this.btnDocentes.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnDocentes.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.btnDocentes.FlatStyle = System.Windows.Forms.FlatStyle.System;
             this.btnDocentes.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnDocentes.Location = new System.Drawing.Point(0, 205);
+            this.btnDocentes.Location = new System.Drawing.Point(0, 243);
             this.btnDocentes.Name = "btnDocentes";
             this.btnDocentes.Size = new System.Drawing.Size(200, 37);
             this.btnDocentes.TabIndex = 1;
@@ -111,14 +129,27 @@ namespace EjemploLibreriaForms.Views
             // 
             // btnAlumnos
             // 
+            this.btnAlumnos.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnAlumnos.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.btnAlumnos.FlatStyle = System.Windows.Forms.FlatStyle.System;
             this.btnAlumnos.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnAlumnos.Location = new System.Drawing.Point(0, 59);
+            this.btnAlumnos.Location = new System.Drawing.Point(0, 118);
             this.btnAlumnos.Name = "btnAlumnos";
             this.btnAlumnos.Size = new System.Drawing.Size(200, 37);
             this.btnAlumnos.TabIndex = 0;
             this.btnAlumnos.Text = "Alumnos";
             this.btnAlumnos.UseVisualStyleBackColor = true;
             this.btnAlumnos.Click += new System.EventHandler(this.btnAlumnos_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::EjemploLibreriaForms.Properties.Resources.logo501;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(194, 82);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
             // 
             // MainView
             // 
@@ -133,6 +164,7 @@ namespace EjemploLibreriaForms.Views
             this.Text = "Vista Principal";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainView_FormClosing);
             this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -146,5 +178,6 @@ namespace EjemploLibreriaForms.Views
         private System.Windows.Forms.Button btn_domicilio;
         private System.Windows.Forms.Button btn_docs;
         private System.Windows.Forms.Button btn_practicas;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
